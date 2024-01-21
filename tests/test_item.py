9b6af52841a854bc23f92381817557f754a3f259
mariasazhina <msazhina23@gmail.com>
2024-01-21 23:49:29 +0200

    added functions for Item class & tests for them

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def item_test():
+    return Item('laptop', 20000, 1)
+
+
+def test_item_init(item_test):
+    assert item_test.name == 'laptop'
+    assert item_test.price == 20000
+    assert item_test.quantity == 1
+
+
+def test_item_total_price(item_test):
+    assert item_test.calculate_total_price() == 20000
+
+def test_item_apply_discount(item_test):
+    assert item_test.apply_discount() == 20000
